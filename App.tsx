5409129acf25f70bc32e3486952610498a8eb770
Patricio Cadenas <plcTools@users.noreply.github.com>
2021-01-22 15:05:51 -0300

    formUser falta type

--- App.tsx
+++ App.tsx
@@ -1,39 +1,57 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
 import { View, Text } from 'react-native';
-import {  Icon } from 'react-native-elements'
+import { Icon } from 'react-native-elements'
+import UserFormScreen from './components/userForm/UserForm'
+import { NavigationScreenProp } from 'react-navigation';
 
-function DetailsScreen() {
+export interface HomeScreenProps{
+  navigation: NavigationScreenProp<any,any>
+};
+
+
+const HomeScreen:React.FC<HomeScreenProps> = ({navigation}) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+      <Text>Home</Text>
+      <Icon
+  raised
+  name='heartbeat'
+  type='font-awesome'
+  color='#f50'
+  onPress={() => navigation.navigate('UserFormScreen')} />
     </View>
   );
 }
+
 const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Details" component={DetailsScreen} options={{
-          title: 'My home',
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerRight: () => (
-            <Icon
-            name="home"
-            color='#fff'
-            onPress={() => alert("hola")}
-        />)
-        }}/>
+      <Stack.Navigator initialRouteName="UserForm">
+        <Stack.Screen name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Petbuddies',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <Icon
+                name="home"
+                color='#fff'
+                onPress={() => alert("hola")}
+              />)
+          }} />
+        <Stack.Screen name="UserForm" component={UserFormScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
