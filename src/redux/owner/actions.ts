a90c9dcee2730610cf4e9c75633951d1e87d9c6e
Nicoglx <nicman_86@hotmail.com>
2021-01-27 13:07:31 -0300

    saco yarn

--- src/redux/owner/actions.ts
+++ src/redux/owner/actions.ts
@@ -5,6 +5,6 @@ type DispatchType = (arg0: OwnerActionsTypes) => OwnerActionsTypes;
 
 
 export const getUserFavorites = (id:string):any => async (dispatch:DispatchType) => {
-    const {data: {favorites}} = await axios.get(`http://localhost:3001/owners/${id}/favorites`)
+    const {data: {favorites}} = await axios.get(`https://henry-petbuddies.herokuapp.com/owners/${id}/favorites`)
     return dispatch({ type: GET_OWNER_FAVORITES, payload: favorites })
 }
--- src/redux/walker/actions.ts
+++ src/redux/walker/actions.ts
@@ -5,6 +5,6 @@ type DispatchType = (arg0: WalkerActionsTypes) => WalkerActionsTypes;
 
 
 export const getWalkers = ():any => async (dispatch:DispatchType) => {
-    const {data} = await axios.get(`http://localhost:3001/walkers`)
+    const {data} = await axios.get(`https://henry-petbuddies.herokuapp.com/walkers`)
     return dispatch({ type: GET_WALKERS, payload: data })
 }
