86049d6b1c7e30b23db19ce351059541189e250e
Patricio Cadenas <plcTools@users.noreply.github.com>
2021-01-31 18:38:25 -0300

    first

---
+++ src/components/BeautySpaScreen/BeautySpaScreen.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { useState } from 'react';
+import  './styles';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
+    Alert
+} from 'react-native';
+import firebase from 'firebase';
+import axios from 'axios';
---
+++ src/components/BeautySpaScreen/SpaCard/spaCard.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { useState } from 'react';
+import './styles';
+import {
+    View,
+ 
+} from 'react-native';
+import firebase from 'firebase';
+import axios from 'axios';
+
+
+function SpaCard () {
+
+return(
+
+<View>
+
+
+
+
+</View>
+
+
+)
+
+
+}
+
+export default SpaCard
+
+// api key: AIzaSyDOdoG3s-oY4SoggPfhVjdTMI7gfKlRDMs
